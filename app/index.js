f88bbe0be1e7c8684ad51779a473d5c512b468a3
Akhilesh yadav <akhileshyadav310596@gmail.com>
2024-02-10 20:49:03 +0530

    setup expo-router

--- app/index.js
+++ app/index.js
@@ -1,21 +1,22 @@
+
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 
-import exercises from "./assets/data/exercises.json"
-import ExerciseListItem from './src/components/ExerciseList';
+import exercises from "../assets/data/exercises.json";
+import ExerciseList from "../components/ExerciseList";
 
 
 
 export default function App() {
- 
+
   return (
     <View style={styles.container}>
     <FlatList
      contentContainerStyle={{gap:5}}
     data={exercises}
     keyExtractor={(item,index) => item.name + index}
-    renderItem={({item,index}) => (
-    <ExerciseListItem item={item}/>
+    renderItem={({item}) => (
+    <ExerciseList item={item}/>
   )}
     />
     <StatusBar style='auto'/>
@@ -26,9 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'gainsboro',
+    backgroundColor: 'ghostwhite',
     justifyContent: 'center',
     padding: 10,
-    paddingTop: 70
+  
   }
 });
--- components/ExerciseList.jsx
+++ components/ExerciseList.jsx
@@ -17,7 +17,18 @@ const styles = StyleSheet.create({
       backgroundColor:'#fff',
       padding: 10,
       borderRadius:10,
-      gap: 5
+      gap: 5,
+      marginHorizontal: 2,
+
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.20,
+      shadowRadius: 1.41,
+
+      elevation: 2,
     },
     exerciseName: {
       fontSize: 20,
